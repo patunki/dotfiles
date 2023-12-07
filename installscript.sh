11184cde266d68ef4806959815d7cb17dfd0572c
Paulus <96471980+patunki@users.noreply.github.com>
2023-12-07 22:25:02 +0200

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-sudo pacman -S xorg-server xorg-xinit xterm awesome polybar picom alacritty gtk3 neofetch rofi nerd-fonts-jetbrains-mono epapirus-icon-theme
+sudo pacman -S xorg-server xorg-xinit xterm awesome polybar picom alacritty gtk3 neofetch rofi epapirus-icon-theme ttf-jetbrains-mono-nerd
 
 mkdir ~/.config
 
 echo “exec awesome” > ~/.xinitrc
 
-cp alacritty ~/.config/
-cp awesome ~/.config/
-cp gtk-3.0 ~/.config/
-cp picom ~/.config/
-cp polybar ~/.config/
-cp rofi ~/.config/
+cp -r alacritty ~/.config/
+cp -r awesome ~/.config/
+cp -r gtk-3.0 ~/.config/
+cp -r picom ~/.config/
+cp -r polybar ~/.config/
+cp -r rofi ~/.config/
 
 sudo chmod +x .config/polybar/launch.sh
 
